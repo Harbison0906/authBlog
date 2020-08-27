cc0137ed1aea3639067061f40806a7bb7364266f
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-08-26 22:03:10 -0500

    added more routes on backend

--- src/server/db/queries/blogs.ts
+++ src/server/db/queries/blogs.ts
@@ -3,7 +3,7 @@ import { Query } from '../';
 
 const all = () => Query('SELECT * FROM Blogs');
 const one = (id: number) => Query('SELECT * FROM Blogs WHERE id=?', [id]);
-const insert = (title: string, author: string, content: string) => Query('INSERT INTO Blogs(title, content) VALUES (?, ?)', [title, content]);
+const insert = (title: string, content: string) => Query('INSERT INTO Blogs(title, content) VALUES (?, ?)', [title, content]);
 const update = (id: number, title: string, content: string) => Query('UPDATE Blogs SET title=?, content=? WHERE id=?', [id, title, content]);
 const destroy = (id: number) => Query('DELETE FROM Blogs WHERE id=?', [id]);
 
--- src/server/routes/api/blogs.ts
+++ src/server/routes/api/blogs.ts
@@ -11,6 +11,7 @@ router.get('/:id?' async (req, res) => {
       res.json(blog)
     } else {
       const blogs = await db.blogs.all();
+      res.json(blogs);
     }
   } catch (error) {
     console.log(error)
@@ -18,3 +19,13 @@ router.get('/:id?' async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => {
+  const blog = req.body;
+  try {
+    const result = await db.blogs.insert(blog.title, blog.content);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500).json('Oops, something went wrong...This is awkward...')
+  }
+})
